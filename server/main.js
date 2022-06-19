65ed3e548662ca0c73db6e79840833d3597e0ed0
AndrewKim330 <rendezvous330@gmail.com>
2022-06-19 16:59:50 +0900

    feat: Construct POST method which adds single post on server

--- server/main.js
+++ server/main.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
 const { hiringRoute } = require('./src/router/hiringRoute');
-app.use('/hiring', hiringRoute);
+const jsonParser = bodyParser.json();
+
+app.use('/hiring', jsonParser, hiringRoute);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
--- server/src/controller/hiringController.js
+++ server/src/controller/hiringController.js
@@ -7,11 +7,22 @@ const { sequelize, User, Notice, Company } = require('../../db/models');
 const allNotices = async (req, res) => {
     console.log('get sequnece - view all notices');
     const allNotices = await Notice.findAll();
-    res.status(200).send(allNotices);
+    const resMsg = {
+        data: allNotices,
+        msg: 'All notices in the server are successfully loaded',
+    };
+    res.status(200).send(resMsg);
 };
 
 const addSingleNotice = async (req, res) => {
     console.log('post sequnece - add single notice');
+    const noticeData = await req.body;
+    await Notice.create(noticeData);
+    const resMsg = {
+        data: null,
+        msg: 'Your notice is successfully added in the server',
+    };
+    res.status(200).send(resMsg);
 };
 
 const updateSingleNotice = async (req, res) => {
